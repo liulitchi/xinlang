aa29db3ec90d7aea951104c822666e762f4f967a
liulitchi <xingzuo88@qq.com>
2021-06-10 06:43:44 +0800

    添加说明文档

--- example/square_field1.c
+++ example/square_field1.c
@@ -1,12 +1,3 @@
-## 沙雕语言 辛语言
-
-这是一门 C 语言的复刻,诞生于2021年6月9日.
-
-第一个程序
-
-> 今有田广十五步，从十六步。问为田几何？
-
-```
 #include <stdio.h>
 #include "/home/litchi/xinlang/include/xinkey.h"
 #include "/home/litchi/xinlang/include/xinstdio.h"
@@ -23,4 +14,4 @@
 	言("田方 %d 平方步.\n", 田);
 	回 0;
 }
-```
+	
---
+++ include/xinsymbol.h
@@ -0,0 +1,24 @@
+#define 为 =
+#define 且 &&
+#define 兼 ||
+#define 取馀于 %
+#define 损 -
+#define 益 +
+#define 乘以 *
+#define 除以 /
+#define 益一 ++
+#define 损一 --
+
+#define 甲 a
+#define 乙 b
+#define 丙 c
+#define 丁 d
+#define 戊 e
+#define 己 f
+#define 庚 g
+#define 辛 h
+#define 壬 i
+#define 癸 j
+#define 甲自乘 (a * a)
+#define 乙自乘 (b * b)
+#define 丙自乘 (c * c)
