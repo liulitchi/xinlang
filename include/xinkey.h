2d9ba31de743bff4c51666675de2964fd3e5627f
liu lizhi <xingzuo88@qq.com>
2021-06-10 05:53:17 +0800

    Create xinkey.h

---
+++ include/xinkey.h
@@ -0,0 +1,33 @@
+#define 天元 main
+#define 回 return
+#define 隐 void
+#define 调 switch
+#define 案 case
+#define 蒂 default
+#define 掣 break
+#define 若 if
+#define 又若 else if
+#define 馀则 else
+#define 逢 while
+#define 历 for
+#define 恒有 const
+#define 架 struct
+#define 度 sizeof
+#define 至 goto
+#define 过 continue
+#define 行 do
+#define 定 static
+#define 合 union
+#define 阴阳 _Bool
+#define 复 _Complex
+#define 虚 _Imaginary
+#define 筹 int
+#define 万 short
+#define 亿 long
+#define 兆 long long
+#define 字 char
+#define 毫 float
+#define 微 double
+#double 翻 unsigned
+#double 笼 enum
+
